da936671007b56f9eff39f81d27c5a8d52f0ba8c
gustavogabana <gustavo_97@protonmail.com>
2024-02-08 19:55:00 -0300

    Added knex as sql query builder

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 const server = fastify()
 
-server.get('/hello', () => {
-    return 'Hello, Node!'
+server.get('/hello', async () => {
+    const tables = await knex('sqlite_schema').select('*')
+    return tables
 })
 
 server.listen({
