8a932448e25c04c54f49725c7e4d64c57d9e6e81
gustavogabana <gustavo_97@protonmail.com>
2024-02-15 21:42:57 -0300

    Tested get route and use of expect.objectContaining

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -19,5 +19,27 @@ describe('Transactions routes', () => {
             type: 'credit'
         })
         .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server).post('/transactions')
+        .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransacionsResponse = await request(app.server).get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+        expect(listTransacionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000
+            })
+        ])
     })
 })
